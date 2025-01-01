e3a01ff2586cbc55f98d860ce111a7ac9be42d85
wangguanghang <guanghangwang@163.com>
2025-01-01 14:37:34 +0800

    babel.config.js配置

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   presets: [
-    '@vue/app'
+    '@vue/app',
+    // 使用官方预设来编译最新的 JavaScript
+    '@babel/preset-env',
+    // 支持 Vue 的 JSX 语法
+    // '@vue/cli-plugin-babel/preset'
+
   ]
 }
