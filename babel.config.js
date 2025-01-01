2c77a8b0ad20ae6d3912184911fe483c1de708ba
wangguanghang <guanghangwang@163.com>
2025-01-01 18:24:47 +0800

    去掉@babel/preset-env编译

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = {
   presets: [
     '@vue/app',
     // 使用官方预设来编译最新的 JavaScript
-    '@babel/preset-env',
+    // '@babel/preset-env',
     // 支持 Vue 的 JSX 语法
     // '@vue/cli-plugin-babel/preset'
 
